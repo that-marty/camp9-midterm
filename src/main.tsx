34c368cb8599072ba6148161103495cd1ba4bf99
Julian Vogel <birdman.coding.1991@gmail.com>
2023-04-17 14:53:34 +0200

    react router

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,32 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { RouterProvider, createBrowserRouter } from 'react-router-dom';
+import './index.css';
+import NavigationLayout from './components/NavigationLayout';
+
+export const router = createBrowserRouter([
+  {
+    path: '/login',
+    element: <h1>Login</h1>,
+  },
+  {
+    path: '/',
+    element: <NavigationLayout />,
+    children: [
+      {
+        index: true,
+        element: <h1>Hai Home</h1>,
+      },
+      {
+        path: '/movies',
+        element: <h1>hellou movies</h1>,
+      },
+    ],
+  },
+]);
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
