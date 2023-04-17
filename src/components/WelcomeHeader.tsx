b7b97baf00e2202508add0d3519fd3bc82f0a494
Student <student@MacBook-Pro-von-Student.local>
2023-04-17 16:55:08 +0200

    somestyling

--- src/components/WelcomeHeader.tsx
+++ src/components/WelcomeHeader.tsx
@@ -1,14 +1,19 @@
 import React from 'react';
+import Avatar from '../assets/Avatar.png';
 
 export default function WelcomeHeader() {
   return (
-    <header className="mx-5 mt-8 flex justify-between">
+    <header className="mx-5 mt-8 flex justify-between bg-[#1C1C27]">
       <div className="flex flex-col justify-between">
-        <h3>Welcome Dan👋</h3>
-        <h1>Let's relax and watch a movie!</h1>
+        <h3 className="font-sans font-bold text-xs text-white opacity-40">
+          Welcome Dan👋
+        </h3>
+        <h1 className="text-white font-bold text-sm">
+          Let's relax and watch a movie!
+        </h1>
       </div>
-      <a>
-        <img src="../assets/Avatar.png"></img>
+      <a href="/">
+        <img src={Avatar}></img>
       </a>
     </header>
   );
