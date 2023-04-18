4ac4b7fc3f1bda14b814cb4855af5d85ca64f9f5
ehidne-mora <mora@slime.garden>
2023-04-18 14:59:27 +0200

    edited the routing in main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -6,30 +6,54 @@ import NavigationLayout from './components/NavigationLayout';
 
 export const router = createBrowserRouter([
   {
-    path: '/login',
-    element: <h1>Login</h1>,
-  },
-  {
     path: '/',
     element: <NavigationLayout />,
     children: [
       {
         index: true,
-        element: <h1>HOME</h1>,
+        element: <h1>HOME</h1>, // insert your page here
       },
       {
         path: '/movies',
-        element: <h1>MOVIES</h1>,
+        element: <h1>MOVIES</h1>, // insert your page here
       },
       {
         path: '/bookmarks',
-        element: <h1>BOOKMARKS</h1>,
+        element: <h1>BOOKMARKS</h1>, // insert your page here
       },
       {
         path: '/account',
-        element: <h1>ACCOUNT</h1>,
+        element: <h1>ACCOUNT</h1>, // insert your page here
       },
     ],
+  },
+  {
+    path: '/login',
+    element: <h1>Login</h1>, // insert your page here
+  },
+  {
+    path: '/genres',
+    element: <h1>GENRES</h1>, // insert your page here
+  },
+  {
+    path: '/movies/:id',
+    element: <h1>MOVIE DETAIL</h1>, // insert your page here
+  },
+  {
+    path: '/cast/:id',
+    element: <h1>CAST AND CREW</h1>, // insert your page here
+  },
+  {
+    path: '/dates',
+    element: <h1>SELECT DATE AND TIME</h1>, // insert your page here
+  },
+  {
+    path: '/seats',
+    element: <h1>SELECT SEATS</h1>, // insert your page here
+  },
+  {
+    path: '/success',
+    element: <h1>BOOKING SUCCESSFULL</h1>, // insert your page here
   },
 ]);
 
