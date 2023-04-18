26ace06af8b72146e09358a989643920ef7d046d
Manuel Pastor Ringuelet <pastorringuelet@hotmail.com>
2023-04-18 15:08:03 +0200

    finished fetching movies

--- src/components/SearchBarRequest.tsx
+++ src/components/SearchBarRequest.tsx
@@ -1,29 +1,46 @@
-import axios from 'axios';
+import axios, { AxiosError } from 'axios';
 import { useState, useEffect } from 'react';
 
-type Movies = {};
+export type Movie = {
+  id: number;
+  title: string;
+  poster_path: string;
+  overview: string;
+};
 
-export default function SearchBarRequest() {
-  const [movies, setMovies] = useState<Movies[]>([]);
+function useQuery<T>(url: string) {
+  const [data, setData] = useState<T | null>(null);
+  const [isLoading, setIsLoading] = useState(true);
+  const [isError, setIsError] = useState<null | string>(null);
 
   useEffect(() => {
-    const apiUrl =
-      'https://api.themoviedb.org/3/movie/popular?api_key=7bdc02c5d27a184488dd56b87a8cad76';
+    async () => {
+      try {
+        const { data } = await axios.get<T>(url);
+        setData(() => data);
+      } catch (err) {
+        const error = err as AxiosError;
+        setIsError(error.message);
+      }
+    };
+  }),
+    [];
 
-    axios
-      .get(apiUrl)
-      .then(response => {
-        setMovies(response.data.results);
-      })
-      .catch(error => {
-        console.log(error);
-      });
-    console.log(movies);
-  }, []);
+  return { data, isLoading, isError };
+}
+
+export default function SearchBarRequest() {
+  const { data, isLoading, isError } = useQuery<Movie[]>(
+    'https://api.themoviedb.org/3/movie/popular?api_key=7bdc02c5d27a184488dd56b87a8cad76'
+  );
+
+  if (isLoading) return <p>Loading...</p>;
+  if (isError) return <p>Error</p>;
+  if (!data) return <p>No Blogpost found</p>;
 
   return (
     <div>
-      {movies.map(movie => (
+      {data.map(movie => (
         <div key={movie.id}>{movie.title}</div>
       ))}
     </div>
