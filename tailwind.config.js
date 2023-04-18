87c7f210a328ea6df2be3ee9490fdedef0dfcef5
ehidne-mora <mora@slime.garden>
2023-04-18 10:17:15 +0200

    Finished the global CSS and Tailwind setup. Also downloaded the fonts to the public/fonts folder. Tailwind setup: added the colors from Figma in the config file. CSS: added the font imports, typography classes and background color of "dark" to the body tag.

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,6 +4,12 @@ export default {
   theme: {
     extend: {
       colors: {
+        dark: '#1C1C27',
+        'dark-light': '#363740',
+        'white-dimmed': 'rgba(255, 255, 255, 0.4)',
+        'white-dimmed-heavy': 'rgba(255, 255, 255, 0.2)',
+        red: '#EF4444',
+        green: '#22C55E',
         yellow: '#FFB43A',
       },
     },
