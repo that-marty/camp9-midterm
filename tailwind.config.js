6a8aba8a0b364e40d3d693c66da9dff39348147d
ehidne-mora <mora@slime.garden>
2023-04-18 11:05:44 +0200

    edited tailwind config variants for white and dark

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,10 +4,17 @@ export default {
   theme: {
     extend: {
       colors: {
-        dark: '#1C1C27',
-        'dark-light': '#363740',
-        'white-dimmed': 'rgba(255, 255, 255, 0.4)',
-        'white-dimmed-heavy': 'rgba(255, 255, 255, 0.2)',
+        dark: {
+          DEFAULT: '#1C1C27',
+          light: '#363740',
+        },
+        white: {
+          DEFAULT: '#FFFFFF',
+          dimmed: {
+            DEFAULT: 'rgba(255, 255, 255, 0.4)',
+            heavy: 'rgba(255, 255, 255, 0.2)',
+          },
+        },
         red: '#EF4444',
         green: '#22C55E',
         yellow: '#FFB43A',
