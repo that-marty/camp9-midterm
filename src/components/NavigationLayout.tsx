d478f4e401e854cbc76164b9413ee39c1059169b
Student <student@MacBook-Pro-von-Student.local>
2023-04-18 14:00:53 +0200

    header-banner component & story with variable username and useravatar image (image couldnt be tested in story as it is a local file)

--- src/components/NavigationLayout.tsx
+++ src/components/NavigationLayout.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Outlet } from 'react-router-dom';
+import Button from './Button';
 
 function NavigationLayout() {
   return (
--- src/components/WelcomeHeader.tsx
+++ src/components/WelcomeHeader.tsx
@@ -1,20 +1,28 @@
 import Avatar from '../assets/Avatar.png';
 import { Link } from 'react-router-dom';
 
-export default function WelcomeHeader() {
+type WelcomeHeader = Props;
+
+interface Props {
+  name?: string;
+  image?: string;
+}
+
+function WelcomeHeader({ name = 'Lui', image = Avatar, ...props }: Props) {
   return (
     <header className="mx-5 mt-8 flex justify-between bg-[#1C1C27]">
       <div className="flex flex-col justify-between">
         <h3 className="font-sans font-bold text-xs text-white opacity-40">
-          Welcome Dan👋
+          Welcome {name}👋
         </h3>
         <h1 className="text-white font-bold text-sm">
           Let's relax and watch a movie!
         </h1>
       </div>
-      <Link to="/">
-        <img alt="avatar" src={Avatar}></img>
-      </Link>
+      <a href="/">
+        <img className="h-10 w-10" alt="avatar" src={image}></img>
+      </a>
     </header>
   );
 }
+export default WelcomeHeader;
