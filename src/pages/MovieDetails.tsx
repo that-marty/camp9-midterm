c6ba4b012e625b1eb8c278f6bb82b6b59e3b4e95
Doug <dmhwebb@gmail.com>
2023-04-20 11:01:10 +0200

    initial page styling

--- src/pages/MovieDetails.tsx
+++ src/pages/MovieDetails.tsx
@@ -2,6 +2,7 @@ import { useParams } from 'react-router-dom';
 import useQuery from '../hook/useQuery';
 import { MovieDetailDbResponse } from '../utilities/types';
 import Button from '../components/Button';
+import MovieDetailHeader from '../components/MovieDetailHeader';
 
 function MovieDetails() {
   const { id } = useParams();
@@ -11,34 +12,36 @@ function MovieDetails() {
   );
 
   return (
-    <>
-      <h1 className="typography-title">Movie Detail </h1>
-      <img
-        className="rounded-md"
-        src={`https://image.tmdb.org/t/p/w500${data?.backdrop_path}`}
-        alt={data?.title}
-      />
-      <h2>{data?.title}</h2>
-      <div>
-        <p>{data?.release_date}</p>
-        {/* <p>{data?.genres }</p> */}
-        <p>{data?.runtime}</p>
-        <p>{data?.vote_average}</p>
-      </div>
-      <div>
+    <article className='pt-7'>
+      <MovieDetailHeader children='Movie Detail' goBackTo='/' svg={true}/>
+      <div className='px-5 flex flex-col'>
+        <img
+          className="rounded-md"
+          src={`https://image.tmdb.org/t/p/w500${data?.backdrop_path}`}
+          alt={data?.title}
+        />
+        <h2 className='typography-title'>{data?.title}</h2>
         <div>
-          <div>Director: Lorem, ipsum.</div>
-          <div>Writer: Lorem, Ipsum.</div>
+          <p className='typography-title'>{data?.release_date}</p>
+          {/* <p>{data?.genres }</p> */}
+          <p className='typography-body'>{data?.runtime}</p>
+          <p className='typography-body'>{data?.vote_average}</p>
         </div>
-        <button>Cast and Crew</button>
+        <div>
+          <div>
+            <div className='typography-body font-bold'>Director: Lorem, ipsum.</div>
+            <div className='typography-body'>Writer: Lorem, Ipsum.</div>
+          </div>
+          <button>Cast and Crew</button>
+        </div>
+        <hr></hr>
+        <h2 className='typography-title'>Synopsis</h2>
+        <p className='typography-body'>{data?.overview}</p>
+        <a href="%">Read more</a>
+        <div className='bg-slate-500 flex-grow'></div>
+        <Button />
       </div>
-      <hr></hr>
-      <h2>Synopsis</h2>
-      <p>{data?.overview}</p>
-      <a href="%">Read more</a>
-      <Button />
-      
-    </>
+    </article>
   );
 }
 
