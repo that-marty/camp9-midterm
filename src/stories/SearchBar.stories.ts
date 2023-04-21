67c91cfbed1203be47dd81bba1abf771760039f1
Doug <dmhwebb@gmail.com>
2023-04-21 11:17:38 +0200

    restore Searchbar story from main

--- src/stories/SearchBar.stories.ts
+++ src/stories/SearchBar.stories.ts
@@ -1,5 +1,5 @@
 import type { Meta, StoryObj } from '@storybook/react';
-import SearchBar from '../components/Searchbar';
+import SearchBar from '../components/SearchBar';
 
 const meta: Meta<typeof SearchBar> = {
   title: 'SearchBar',
